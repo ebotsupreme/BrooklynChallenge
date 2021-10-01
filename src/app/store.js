8d6c60b006f041e4828c1ea8f161634df85e1ffb
Eddie Jung <ejung716@gmail.com>
2021-10-01 13:09:16 -0700

    Add teamSlice and teamReducer to store.

--- src/app/store.js
+++ src/app/store.js
@@ -2,12 +2,14 @@ import {configureStore} from '@reduxjs/toolkit';
 import {teamsApi} from '../services/teams';
 import {playersApi} from '../services/players';
 // import {playerImagesApi} from '../services/playerImage';
+import teamReducer from '../features/team/teamSlice';
 
 export const store = configureStore({
   reducer: {
     [teamsApi.reducerPath]: teamsApi.reducer,
     [playersApi.reducerPath]: playersApi.reducer,
     // [playerImagesApi.reducerPath]: playerImagesApi.reducer,
+    team: teamReducer,
   },
   middleware: getDefaultMiddleware =>
     getDefaultMiddleware({
--- src/features/team/teamSlice.js
+++ src/features/team/teamSlice.js
@@ -1,9 +1,30 @@
 import {createSlice} from '@reduxjs/toolkit';
 
 const initialState = {
+  isLoading: false,
+  isLoaded: false,
+  error: false,
   teamOneIsActive: false,
   teamTwoIsActive: false,
   teamThreeIsActive: false,
+  teamOne: {
+    id: '',
+    name: '',
+    city: '',
+    players: [],
+  },
+  teamTwo: {
+    id: '',
+    name: '',
+    city: '',
+    players: [],
+  },
+  teamThree: {
+    id: '',
+    name: '',
+    city: '',
+    players: [],
+  },
   teams: [
     {
       teamOne: {
@@ -63,3 +84,50 @@ players: [
           },
         ],
 */
+
+export const teamSlice = createSlice({
+  name: 'team',
+  initialState,
+  reducers: {
+    startLoading: state => {
+      state.isLoading = true;
+      state.isLoaded = false;
+    },
+    hasError: (state, action) => {
+      state.error = action.payload;
+      state.isLoading = false;
+      state.isLoaded = false;
+    },
+    updateTeamOne: (state, action) => {
+      state.teamOne = action.payload;
+      state.teamOneIsActive = true;
+      state.isLoading = false;
+      state.isLoaded = true;
+    },
+    updateTeamTwo: (state, action) => {
+      state.teamOne = action.payload;
+      state.teamOneIsActive = true;
+      state.isLoading = false;
+      state.isLoaded = true;
+    },
+    updateTeamThree: (state, action) => {
+      state.teamOne = action.payload;
+      state.teamOneIsActive = true;
+      state.isLoading = false;
+      state.isLoaded = true;
+    },
+    // teamSuccess: (state, action) => {
+    //   state.
+    // },
+  },
+});
+
+export const {
+  startLoading,
+  hasError,
+  updateTeamOne,
+  updateTeamTwo,
+  updateTeamThree,
+} = teamSlice.actions;
+
+export default teamSlice.reducer;
